15cf793a7852041c14b7eccecc9152a341a0c8a6
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-09-27 17:05:55 +0200

    fix init

--- jupyterlab_quickopen/__init__.py
+++ jupyterlab_quickopen/__init__.py
@@ -1,21 +1,20 @@
 """Registers the jupyterlab front and backend quickopen extensions"""
-import json
-from pathlib import Path
-
-from ._version import __version__
-
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
-
-def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": data["name"]}]
-
+try:
+    from ._version import __version__
+except ImportError:
+    # Fallback when using the package in dev mode without installing
+    # in editable mode with pip. It is highly recommended to install
+    # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
+    import warnings
+    warnings.warn("Importing 'jupyterlab_quickopen' outside a proper installation.")
+    __version__ = "dev"
 
 from .handler import QuickOpenHandler
 from jupyter_server.utils import url_path_join
+
+
+def _jupyter_labextension_paths():
+    return [{"src": "labextension", "dest": "jupyterlab-quickopen"}]
 
 
 def _jupyter_server_extension_points():
@@ -54,7 +53,3 @@ def _load_jupyter_server_extension(server_app):
         f"Registered QuickOpenHandler extension at URL path {route_pattern} "
         f"to serve results of scanning local path {server_app.notebook_dir}"
     )
-
-
-# For backward compatibility with notebook server - useful for Binder/JupyterHub
-load_jupyter_server_extension = _load_jupyter_server_extension
